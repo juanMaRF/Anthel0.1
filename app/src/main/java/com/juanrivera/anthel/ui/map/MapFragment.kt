3b1bed03f8f4539b604e905c154d6f562c1d1fb3
Juan Ma <juanm.rivera@udea.edu.co>
2023-06-05 10:54:06 -0500

    Se crea fragment de mapa, se agrega al menu de navegacion.

--- app/src/main/java/com/juanrivera/anthel/ui/map/MapFragment.kt
+++ app/src/main/java/com/juanrivera/anthel/ui/map/MapFragment.kt
@@ -1,6 +1,37 @@
 package com.juanrivera.anthel.ui.map
 
 import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import com.juanrivera.anthel.databinding.FragmentMapBinding
 
 class MapFragment: Fragment() {
+
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val MapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        MapViewModel.text.observe(viewLifecycleOwner){
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/juanrivera/anthel/ui/map/MapViewModel.kt
+++ app/src/main/java/com/juanrivera/anthel/ui/map/MapViewModel.kt
@@ -1,6 +1,12 @@
 package com.juanrivera.anthel.ui.map
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MapViewModel:ViewModel() {
+    private val _text = MutableLiveData<String>().apply {
+        value = "Aqui va el mapa"
+    }
+    val text: LiveData<String> = _text
 }
