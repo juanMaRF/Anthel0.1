cd3e129ae9e757eefce36e31a6bc6561a1b205bb
Juan Ma <juanm.rivera@udea.edu.co>
2023-06-07 13:52:32 -0500

    Se hacen los child fragments para el fragment de perfil. - se arregla el childfragment de reserva

--- app/src/main/java/com/juanrivera/anthel/ui/dashboard/insidefrag/ReservaFragment.kt
+++ app/src/main/java/com/juanrivera/anthel/ui/dashboard/insidefrag/ReservaFragment.kt
@@ -1,7 +1,33 @@
 package com.juanrivera.anthel.ui.dashboard.insidefrag
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.juanrivera.anthel.databinding.FragmentReservaBinding
 
 class ReservaFragment: Fragment() {
+    private var _binding: FragmentReservaBinding? = null
 
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentReservaBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
